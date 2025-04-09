dde4bc0b2b0f7cdc315ed80498c080f45a9bf363
darryl <darrylrambi@gmail.com>
2025-04-09 21:02:16 +0700

    finish layouting main page

--- resources/views/main-page.blade.php
+++ resources/views/main-page.blade.php
@@ -1,3 +1,40 @@
-<div>
-    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>SMK </title>
+  <!-- External buat background -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/particlesjs/2.2.2/particles.min.js"></script>
+  
+  <!-- Conect CSS bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+  integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+  
+  <!-- Connect CSS -->
+  <link rel="stylesheet" href="{{ asset('css/main-page.css') }}">
+  
+  <!-- Import Fonts -->
+  <link
+  href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Merriweather:wght@400;700&display=swap"
+  rel="stylesheet">
+</head>
+
+<body>
+  <!-- Wrapper -->
+  <div class="container">
+    <div class="Main content"></div>
+    <div class="Slider"></div>
+  </div>
+
+  <!-- Connect Bootsrap bundle -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+
+  <!-- Connect Custom JS -->
+  <script src="{{ asset('js/darryl.js') }}"></script>
+</body>
+
+</html>
