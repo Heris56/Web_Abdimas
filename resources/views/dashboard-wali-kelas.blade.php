8e5f54309ddc3a8a7459fbd06eb662c79b2d52f7
Dafrisu <daffaraymisuandi@gmail.com>
2025-05-16 20:23:41 +0700

    Fixing navbarr

--- resources/views/dashboard-wali-kelas.blade.php
+++ resources/views/dashboard-wali-kelas.blade.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Dashboard Presensi</title>
+  <link rel="stylesheet" href="{{ asset('css/login-page.css') }}">
   <link rel="stylesheet" href="{{ asset('css/dashboard-wali-kelas.css') }}">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/particlesjs/2.2.2/particles.min.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
