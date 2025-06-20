036c22f658ba21b8188d609d827afc0df4ab92bd
darryl <darrylrambi@gmail.com>
2025-06-20 15:52:10 +0700

    fix css

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,11 +1,10 @@
-<div>
     <!-- Navbar -->
     <nav class="navbar container-fluid fixed-top">
 
         <!-- navigate to home/dashboard by clicking logo/name -->
         <a class="logo" href="{{ route('landing') }}">
-            <img src="{{ asset('images/logo_pgri.png') }}" alt="Logo" width="64" height="64" class="logo-img d-inline-block" />
-            SMK PGRI 35
+            <img src="{{ asset('images/logo_pgri.png') }}" alt="Logo" width="64" height="64"
+                class="logo-img d-inline-block" />
         </a>
 
         <!-- Button login/register -->
@@ -14,5 +13,16 @@
                 Cari data Siswa
             </a>
         </div>
+        <div class="btn-group">
+            <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown"
+                data-bs-display="static" aria-expanded="false">
+                <i class="bi bi-person-fill"></i>
+                Staff
+            </button>
+            <ul class="dropdown-menu dropdown-menu-lg-end">
+                <li><button class="dropdown-item" type="button">Action</button></li>
+                <li><button class="dropdown-item" type="button">Another action</button></li>
+                <li><button class="dropdown-item" type="button">Something else here</button></li>
+            </ul>
+        </div>
     </nav>
-</div>
