f54eaadad52a0b603d78f1849301a96d62de78e3
darryl <darrylrambi@gmail.com>
2025-06-26 21:19:03 +0700

    fix inherit colors in navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -10,7 +10,7 @@
     <a class="logo" href="{{ route('landing') }}">
         <img src="{{ asset('images/logo_pgri.png') }}" alt="Logo" width="64" height="64"
             class="logo-img d-inline-block" />
-        SMK PGRI 35
+        <span>SMK PGRI 35</span>
     </a>
 
     <div class="search-container ms-auto">
