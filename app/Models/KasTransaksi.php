df2cca3d0922b7cbfe69fbe9d56921d2ffeb6482
Heris56 <haikalrisnandar82@gmail.com>
2025-10-01 23:04:17 +0700

    init model kas

---
+++ app/Models/KasTransaksi.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class KasTransaksi extends Model
+{
+    protected $table = 'cashflow_kas_transaksi';
+    protected $primaryKey = "id_kas_transaksi";
+    protected $fillable = [
+        "id_kas",
+        "sumber",
+        "id_sumber",
+        "tanggal",
+        "keterangan",
+        "debit",
+        "kredit",
+        "saldo_akhir"
+    ];
+
+    public function kas(){
+        return $this->belongsTo(Kas::class, "id_kas", "id_kas");
+    }
+}
