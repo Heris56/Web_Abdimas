80bd2b2b9977f8ca54c0362c49ace6f477ee2732
darryl <darrylrambi@gmail.com>
2025-10-03 13:30:24 +0700

    make tipe pembayaran default to 7 data

--- database/factories/KasFactory.php
+++ database/factories/KasFactory.php
@@ -19,7 +19,7 @@ class KasFactory extends Factory
     public function definition(): array
     {
         return [
-            'id_tipe_pembayaran' => TipePembayaran::factory(), // or an existing tipe_pembayaran ID
+            'id_tipe_pembayaran' => TipePembayaran::inRandomOrder()->value('id_tipe_pembayaran') ?? null, // or an existing tipe_pembayaran ID
             'nama_kas' => $this->faker->word() . ' Cash',
             'saldo' => $this->faker->randomFloat(2, 0, 1000000), // random balance
         ];
--- database/seeders/TipePembayaranSeeder.php
+++ database/seeders/TipePembayaranSeeder.php
@@ -13,9 +13,6 @@ class TipePembayaranSeeder extends Seeder
      */
     public function run(): void
     {
-        // Generate dummy random data
-        TipePembayaran::factory()->count(10)->create();
-
         // Tambahkan beberapa data fix
         TipePembayaran::create([
             'nama_tipe' => 'SPP',
@@ -31,6 +28,43 @@ class TipePembayaranSeeder extends Seeder
             'is_sekali_bayar' => true,
             'nominal' => 2500000,
             'keterangan' => 'Dibayarkan sekali saat masuk',
+        ]);
+
+        TipePembayaran::create([
+            'nama_tipe' => 'PSB',
+            'tipe_periodik' => 'bulanan',
+            'is_bulanan' => true,
+            'nominal' => 500000,
+            'keterangan' => 'Pembayaran SPP per bulan',
+        ]);
+
+        TipePembayaran::create([
+            'nama_tipe' => 'PTS',
+            'tipe_periodik' => 'bulanan',
+            'is_bulanan' => true,
+            'nominal' => 500000,
+            'keterangan' => 'Pembayaran SPP per bulan',
+        ]);
+        TipePembayaran::create([
+            'nama_tipe' => 'PAS',
+            'tipe_periodik' => 'bulanan',
+            'is_bulanan' => true,
+            'nominal' => 500000,
+            'keterangan' => 'Pembayaran SPP per bulan',
+        ]);
+        TipePembayaran::create([
+            'nama_tipe' => 'PKL',
+            'tipe_periodik' => 'bulanan',
+            'is_bulanan' => true,
+            'nominal' => 500000,
+            'keterangan' => 'Pembayaran SPP per bulan',
+        ]);
+        TipePembayaran::create([
+            'nama_tipe' => 'Kunjungan Industri',
+            'tipe_periodik' => 'bulanan',
+            'is_bulanan' => true,
+            'nominal' => 500000,
+            'keterangan' => 'Pembayaran SPP per bulan',
         ]);
     }
 }
