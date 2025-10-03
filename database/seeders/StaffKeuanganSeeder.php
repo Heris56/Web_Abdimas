322185b15f439befefe7047e3a168eef87944221
darryl <darrylrambi@gmail.com>
2025-10-03 12:07:09 +0700

    finished making seeder, migrations, factory for staff keuangan

---
+++ database/seeders/StaffKeuanganSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\StaffKeuangan;
+use Illuminate\Container\Attributes\DB;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class StaffKeuanganSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        StaffKeuangan::create([
+            [
+                'nama' => 'Darryl Rambi',
+                'email' => 'darryl@example.com',
+                'password' => Hash::make('rahasia123'),
+                'status' => 'active',
+            ],
+            [
+                'nama' => 'Dafa Raimi',
+                'email' => 'dafa@example.com',
+                'password' => Hash::make('rahasia123'),
+                'status' => 'active',
+            ],
+            [
+                'nama' => 'Raphael Permana',
+                'email' => 'raphael@example.com',
+                'password' => Hash::make('rahasia123'),
+                'status' => 'active',
+            ],
+            [
+                'nama' => 'Haikal Risnandar',
+                'email' => 'haikal@example.com',
+                'password' => Hash::make('rahasia123'),
+                'status' => 'active',
+            ],
+        ]);
+    }
+}
