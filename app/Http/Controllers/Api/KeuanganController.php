4a3488a9957c37e82fcdff1d9812e46379e66b52
Haikal Risnandar <haikalrisnandar82@gmail.com>
2025-10-26 22:43:54 +0700

    fix api dikit

--- app/Http/Controllers/Api/KeuanganController.php
+++ app/Http/Controllers/Api/KeuanganController.php
@@ -1158,10 +1158,11 @@ class KeuanganController extends Controller
         DB::beginTransaction();
         try {
             StaffKeuangan::create([
-                'nama' => 'Raphael Permana Barus',
-                'email' => 'raphael@example.com',
+                'nama' => 'Dini Insan Fatonah',
+                'email' => 'dini.insanpatonah62@gmail.com',
                 'password' => Hash::make('rahasia123'),
-                'status' => 'Aktif'
+                'status' => 'Aktif',
+                'role' => "Auditor",
             ]);
 
             DB::commit();
